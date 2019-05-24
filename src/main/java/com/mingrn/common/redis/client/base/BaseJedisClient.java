f543e69c135aa3694a2bea65956f0ca21a26d266
MinGRn <MinGRn97@gmail.com>
2019-05-24 10:11:17 +0800

    代码 优化;

--- src/main/java/com/mingrn/common/redis/client/base/BaseJedisClient.java
+++ src/main/java/com/mingrn/common/redis/client/base/BaseJedisClient.java
@@ -11,172 +11,172 @@ import redis.clients.jedis.Jedis;
  */
 public abstract class BaseJedisClient implements BaseJedisRepository {
 
-	@Override
-	public Long delete(String... keys) {
-		Jedis jedis = null;
-		try {
-			jedis = AbstractJedisConfig.acquireResource();
-			return jedis.del(keys);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		} finally {
-			AbstractJedisConfig.releaseResource(jedis);
-		}
-	}
+    @Override
+    public Long delete(String... keys) {
+        Jedis jedis = null;
+        try {
+            jedis = AbstractJedisConfig.acquireResource();
+            return jedis.del(keys);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            AbstractJedisConfig.releaseResource(jedis);
+        }
+    }
 
-	@Override
-	public String rename(String key, String newKey) {
-		Jedis jedis = null;
-		try {
-			jedis = AbstractJedisConfig.acquireResource();
-			return jedis.rename(key, newKey);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		} finally {
-			AbstractJedisConfig.releaseResource(jedis);
-		}
-	}
+    @Override
+    public String rename(String key, String newKey) {
+        Jedis jedis = null;
+        try {
+            jedis = AbstractJedisConfig.acquireResource();
+            return jedis.rename(key, newKey);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            AbstractJedisConfig.releaseResource(jedis);
+        }
+    }
 
-	@Override
-	public Long renameNx(String key, String newKey) {
-		Jedis jedis = null;
-		try {
-			jedis = AbstractJedisConfig.acquireResource();
-			return jedis.renamenx(key, newKey);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		} finally {
-			AbstractJedisConfig.releaseResource(jedis);
-		}
-	}
+    @Override
+    public Long renameAndNotExist(String key, String newKey) {
+        Jedis jedis = null;
+        try {
+            jedis = AbstractJedisConfig.acquireResource();
+            return jedis.renamenx(key, newKey);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            AbstractJedisConfig.releaseResource(jedis);
+        }
+    }
 
-	@Override
-	public Long expire(String key, int seconds) {
-		Jedis jedis = null;
-		try {
-			jedis = AbstractJedisConfig.acquireResource();
-			return jedis.expire(key, seconds);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		} finally {
-			AbstractJedisConfig.releaseResource(jedis);
-		}
-	}
+    @Override
+    public Long expire(String key, int seconds) {
+        Jedis jedis = null;
+        try {
+            jedis = AbstractJedisConfig.acquireResource();
+            return jedis.expire(key, seconds);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            AbstractJedisConfig.releaseResource(jedis);
+        }
+    }
 
-	@Override
-	public Long expireAt(String key, long timestamp) {
-		Jedis jedis = null;
-		try {
-			jedis = AbstractJedisConfig.acquireResource();
-			return jedis.expireAt(key, timestamp);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		} finally {
-			AbstractJedisConfig.releaseResource(jedis);
-		}
-	}
+    @Override
+    public Long expireAtTimeStamp(String key, long timestamp) {
+        Jedis jedis = null;
+        try {
+            jedis = AbstractJedisConfig.acquireResource();
+            return jedis.expireAt(key, timestamp);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            AbstractJedisConfig.releaseResource(jedis);
+        }
+    }
 
-	@Override
-	public Long pExpire(String key, long milliseconds) {
-		Jedis jedis = null;
-		try {
-			jedis = AbstractJedisConfig.acquireResource();
-			return jedis.pexpire(key, milliseconds);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		} finally {
-			AbstractJedisConfig.releaseResource(jedis);
-		}
-	}
+    @Override
+    public Long expireOfMillis(String key, long milliseconds) {
+        Jedis jedis = null;
+        try {
+            jedis = AbstractJedisConfig.acquireResource();
+            return jedis.pexpire(key, milliseconds);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            AbstractJedisConfig.releaseResource(jedis);
+        }
+    }
 
-	@Override
-	public Long pExpireAt(String key, long millisecondsTimestamp) {
-		Jedis jedis = null;
-		try {
-			jedis = AbstractJedisConfig.acquireResource();
-			return jedis.pexpireAt(key, millisecondsTimestamp);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		} finally {
-			AbstractJedisConfig.releaseResource(jedis);
-		}
-	}
+    @Override
+    public Long expireAtMillisTimeStamp(String key, long millisecondsTimestamp) {
+        Jedis jedis = null;
+        try {
+            jedis = AbstractJedisConfig.acquireResource();
+            return jedis.pexpireAt(key, millisecondsTimestamp);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            AbstractJedisConfig.releaseResource(jedis);
+        }
+    }
 
-	@Override
-	public Long persist(String key) {
-		Jedis jedis = null;
-		try {
-			jedis = AbstractJedisConfig.acquireResource();
-			return jedis.persist(key);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		} finally {
-			AbstractJedisConfig.releaseResource(jedis);
-		}
-	}
+    @Override
+    public Long persist(String key) {
+        Jedis jedis = null;
+        try {
+            jedis = AbstractJedisConfig.acquireResource();
+            return jedis.persist(key);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            AbstractJedisConfig.releaseResource(jedis);
+        }
+    }
 
-	@Override
-	public Long ttl(String key) {
-		Jedis jedis = null;
-		try {
-			jedis = AbstractJedisConfig.acquireResource();
-			return jedis.ttl(key);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		} finally {
-			AbstractJedisConfig.releaseResource(jedis);
-		}
-	}
+    @Override
+    public Long ttl(String key) {
+        Jedis jedis = null;
+        try {
+            jedis = AbstractJedisConfig.acquireResource();
+            return jedis.ttl(key);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            AbstractJedisConfig.releaseResource(jedis);
+        }
+    }
 
-	@Override
-	public Long pTtl(String key) {
-		Jedis jedis = null;
-		try {
-			jedis = AbstractJedisConfig.acquireResource();
-			return jedis.pttl(key);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		} finally {
-			AbstractJedisConfig.releaseResource(jedis);
-		}
-	}
+    @Override
+    public Long ttlOfMillis(String key) {
+        Jedis jedis = null;
+        try {
+            jedis = AbstractJedisConfig.acquireResource();
+            return jedis.pttl(key);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            AbstractJedisConfig.releaseResource(jedis);
+        }
+    }
 
-	@Override
-	public Long exists(String... keys) {
-		Jedis jedis = null;
-		try {
-			jedis = AbstractJedisConfig.acquireResource();
-			return jedis.exists(keys);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		} finally {
-			AbstractJedisConfig.releaseResource(jedis);
-		}
-	}
+    @Override
+    public Long exists(String... keys) {
+        Jedis jedis = null;
+        try {
+            jedis = AbstractJedisConfig.acquireResource();
+            return jedis.exists(keys);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            AbstractJedisConfig.releaseResource(jedis);
+        }
+    }
 
-	@Override
-	public String type(String key) {
-		Jedis jedis = null;
-		try {
-			jedis = AbstractJedisConfig.acquireResource();
-			return jedis.type(key);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		} finally {
-			AbstractJedisConfig.releaseResource(jedis);
-		}
-	}
+    @Override
+    public String type(String key) {
+        Jedis jedis = null;
+        try {
+            jedis = AbstractJedisConfig.acquireResource();
+            return jedis.type(key);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            AbstractJedisConfig.releaseResource(jedis);
+        }
+    }
 
-	@Override
-	public String keyEncoding(String key) {
-		Jedis jedis = null;
-		try {
-			jedis = AbstractJedisConfig.acquireResource();
-			return jedis.objectEncoding(key);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		} finally {
-			AbstractJedisConfig.releaseResource(jedis);
-		}
-	}
+    @Override
+    public String keyEncoding(String key) {
+        Jedis jedis = null;
+        try {
+            jedis = AbstractJedisConfig.acquireResource();
+            return jedis.objectEncoding(key);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            AbstractJedisConfig.releaseResource(jedis);
+        }
+    }
 }
